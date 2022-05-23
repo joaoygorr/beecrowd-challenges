e9a97c80794c048dd841baf5fe4791f090473602
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-05-23 12:25:29 -0400

    .

--- 1-Iniciante/1008.py
+++
@@ -1,6 +0,0 @@
-num = int(input())
-numHrs = int(input())
-valorHrs = float(input())
-
-print(f"NUMBER = {num}")
-print(f"SALARY = U$ {numHrs * valorHrs:.2f}")
--- 1-Iniciante/1009.py
+++
@@ -1,5 +0,0 @@
-nome = str(input())
-sFixo = float(input())
-totVenda = float(input())
-
-print(f"TOTAL = R$ {sFixo+((totVenda*15)/100):.2f}")
