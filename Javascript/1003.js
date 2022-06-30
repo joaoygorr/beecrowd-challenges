9e8a25b0730655651ecc38d88f71d783dbc41c2a
João Ygor <ygorvieira2002@gmail.com>
2022-06-30 18:15:27 -0400

    Soma Simples

---
+++ Javascript/1003.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+let lines = input.split('\n');
+
+let a = parseInt(lines.shift());
+let b = parseInt(lines.shift());
+
+console.log("SOMA =", a + b);
